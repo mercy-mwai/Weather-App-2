c82d1b546a81ef89a1bcdfb2fdde21b024f8351f
mercy-mwai <mercyneemam@gmail.com>
2024-08-14 11:09:25 +0300

    commit message

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let weather={
     adelaide: {
-        temp: 22,
+        temp: 22.6,
         humidity: 21,
         wind: 12,
     },
@@ -10,13 +10,22 @@ let weather={
         wind: 4,
     },
     lisbon: {
-        temp: 17,
+        temp: 17.4,
         humidity: 21,
         wind: 13,
     },
     beijing: {
-        temp: 18,
+        temp: 18.7,
         humidity: 24,
         wind: 13,
     }
+}
+let city=prompt("Enter a city?");
+city=city.toLowerCase();
+if(weather[city] !== undefined){
+    let temperature=weather[city].temp;
+    let humidity=weather[city].humidity;
+    let wind=weather[city].wind;
+    let celsiusTemperature = Math.round(temperature);
+    let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
 }
