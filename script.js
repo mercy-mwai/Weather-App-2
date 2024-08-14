4b722b7de27a060105a71d555f445ef7e535753d
mercy-mwai <mercyneemam@gmail.com>
2024-08-14 10:52:10 +0300

    commit message

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-let city=prompt("What city are you from");
-let name=Prompt("What is your name?");
+let weather={
+    adelaide: {
+        temp: 22,
+        humidity: 21,
+        wind: 12,
+    },
+    barcelona: {
+        temp:19,
+        humidity: 31,
+        wind: 4,
+    },
+    lisbon: {
+        temp: 17,
+        humidity: 21,
+        wind: 13,
+    },
+    beijing: {
+        temp: 18,
+        humidity: 24,
+        wind: 13,
+    }
+}
