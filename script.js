5a08ab3ab78d37f876a7a8e6dd62b4e3116407ff
mercy-mwai <mercyneemam@gmail.com>
2024-08-26 12:06:49 +0300

    small changes

--- script.js
+++ script.js
@@ -7,12 +7,22 @@
     let searchButton=document.querySelector('.search-button');
     searchButton.addEventListener("click", contactBtn);
     
-    function currentDayOfWeek(){
-        let date=new Date();  
+    function currentDayOfWeek(){ 
         let DaysOfWeek=["Monday","Tuesday","Wednesday", "Thursday", "Friday", "Sturday","Sunday"];
         let dayName = daysOfWeek[date.getDay()];
         let hours=date.getHours();
         let minutes=date.getMinutes(); 
-        const formattedTime = `${dayName} ${hours}:${minutes}`;
+        if (minutes < 10) {
+            minutes = `0${minutes}`;
+          }
+        
+          if (hours < 10) {
+            hours = `0${hours}`;
+          }
+        
+        return `${dayName}:${hours}:${minutes}`;
              
     };
+    let currentDateElement=document.getElementById('current-date');
+    let currentDate=new Date();
+    currentDateELement.innerHTML = currentDayOfWeek(currentDate);
