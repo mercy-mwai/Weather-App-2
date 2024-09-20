2705e713db799b703bac0f2f592ca17cf80e33cb
mercy-mwai <mercyneemam@gmail.com>
2024-09-20 12:47:27 +0300

    commit message

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,8 +2,15 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = response.data.city;
+  let descriptionElement=document.querySelector("#description");
+  let humidityElement=document.querySelector("#humidity");
+  let windSpeedElement=document.querySelector("#wind-speed");
+
+  cityElement.innerHTML=response.data.city;
   temperatureElement.innerHTML = temperature;
+  descriptionElement.innerHTML=response.data.condition.description;
+  humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML=`${response.data.wind.speed} Km\h`
 }
 
 function search(event) {
