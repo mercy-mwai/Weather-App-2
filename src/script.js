6db7dd2264594a702fc57d88ee9d6655efbcbf10
mercy-mwai <mercyneemam@gmail.com>
2024-09-24 14:17:49 +0300

    Added the forecast API to my project

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-function displayTemperature(response) {
+function displayCurrentWeather(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
@@ -6,8 +6,8 @@ function displayTemperature(response) {
   let humidityElement=document.querySelector("#humidity");
   let windSpeedElement=document.querySelector("#wind-speed");
   let iconElement=document.querySelector("#icon");
+  let date = new Date(response.data.time * 1000);
   let iconUrl = response.data.condition["icon_url"]
-
 
   cityElement.innerHTML=response.data.city;
   temperatureElement.innerHTML = temperature;
@@ -15,6 +15,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML=`${response.data.wind.speed} Km\h`;
   iconElement.innerHTML = `<img src="${iconUrl}" class="weather-icon"/>`;
+
+  getForecast(response.data.city);
 };
 
 function formatDate(date) {
@@ -48,39 +50,55 @@ function formatDate(date) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayCurrentWeather);
   };
 
-  function displayForecast() {
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-    let forecastHtml = "";
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   
-    days.forEach(function (day) {
-      forecastHtml =
-        forecastHtml +
-        `
-        <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">🌤️</div>
-          <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-temperature">
-              <strong>15º</strong>
-            </div>
-            <div class="weather-forecast-temperature">9º</div>
-          </div>
-        </div>
-      `;
-    });
-  
-    let forecastElement = document.querySelector("#forecast");
-    forecastElement.innerHTML = forecastHtml;
+    return days[date.getDay()];
   };
-  
+
   function search(event) {
     event.preventDefault();
     let searchInputElement = document.querySelector("#search-input");
     searchCity(searchInputElement.value);
   };
+
+  function getForecast(city){
+      let apiKey="ataf73cb6640fo53e168b11f0d6bb9e4";
+      let apiUrl=`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+      axios.get(apiUrl).then(displayForecast);
+    };
+
+  function displayForecast(response) {
+    
+    let forecastHtml = "";
+  
+    response.data.daily.forEach(function (day ,index) {
+      if (index < 5){
+      forecastHtml =
+        forecastHtml +
+        `
+        <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <img  src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>${Math.round(day.temperature.maximum)}º</strong>
+            </div>
+            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
+          </div>
+        </div>
+      `;
+    }
+    });
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+  };
+  
+  
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
@@ -90,4 +108,3 @@ let currentDate = new Date();
 currentDateELement.innerHTML = formatDate(currentDate);
 
 searchCity("Paris");
-displayForecast();
